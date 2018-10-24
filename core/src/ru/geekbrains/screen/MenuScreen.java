9448c69f51dfbff08f3f5846a4e77babdd16d5af
Alexey Kutepov <alkutepov@gmail.com>
2018-10-24 21:40:54 +0300

    Урок3

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -10,18 +10,22 @@ import ru.geekbrains.base.Base2DScreen;
 
 public class MenuScreen extends Base2DScreen {
 
-    private SpriteBatch batch;
     private Texture img;
 
     private Vector2 pos;
+    private Vector2 touch;
+    private Vector2 v;
+    private Vector2 buf;
 
 
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
         pos = new Vector2(0,0);
+        touch = new Vector2();
+        v = new Vector2();
+        buf = new Vector2();
     }
 
     @Override
@@ -29,23 +33,28 @@ public class MenuScreen extends Base2DScreen {
         super.render(delta);
         Gdx.gl.glClearColor(0.128f, 0.53f, 0.9f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.getProjectionMatrix().idt();
-
+        buf.set(touch);
+        if (buf.sub(pos).len() > v.len()) {
+            pos.add(v);
+        } else {
+            pos.set(touch);
+        }
         batch.begin();
-        batch.draw(img, -1f,-1f, 2f, 2f);
+        batch.draw(img, pos.x, pos.y, 0.5f, 0.5f);
         batch.end();
 
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
         img.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+    public boolean touchDown(Vector2 touch, int pointer) {
+        this.touch = touch;
+        v.set(touch.cpy().sub(pos).scl(0.01f));
         return false;
     }
 }
