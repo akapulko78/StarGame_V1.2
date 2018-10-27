003011176716aaa4c7a7d71a217616da63277236
Alexey Kutepov <alkutepov@gmail.com>
2018-10-27 12:18:17 +0300

    Урок 4 (часть 1)

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -3,29 +3,25 @@ package ru.geekbrains.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.Base2DScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
 
 public class MenuScreen extends Base2DScreen {
 
-    private Texture img;
 
-    private Vector2 pos;
-    private Vector2 touch;
-    private Vector2 v;
-    private Vector2 buf;
+    private Texture bgTexture;
+    private Background background;
 
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
-        pos = new Vector2(0,0);
-        touch = new Vector2();
-        v = new Vector2();
-        buf = new Vector2();
+        bgTexture = new Texture("bg.png");
+        background = new Background(new TextureRegion(bgTexture));
     }
 
     @Override
@@ -33,28 +29,25 @@ public class MenuScreen extends Base2DScreen {
         super.render(delta);
         Gdx.gl.glClearColor(0.128f, 0.53f, 0.9f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        buf.set(touch);
-        if (buf.sub(pos).len() > v.len()) {
-            pos.add(v);
-        } else {
-            pos.set(touch);
-        }
         batch.begin();
-        batch.draw(img, pos.x, pos.y, 0.5f, 0.5f);
+        background.draw(batch);
         batch.end();
 
     }
 
     @Override
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
+    }
+
+    @Override
     public void dispose() {
-        img.dispose();
+        bgTexture.dispose();
         super.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer) {
-        this.touch = touch;
-        v.set(touch.cpy().sub(pos).scl(0.01f));
         return false;
     }
 }
