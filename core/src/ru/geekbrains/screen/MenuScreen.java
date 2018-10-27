2cf4bdb8a7d9c0675bfca8f7231cf1f3bd8eb767
Alexey Kutepov <alkutepov@gmail.com>
2018-10-27 12:54:04 +0300

    Урок 4 (часть 2)

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -3,18 +3,24 @@ package ru.geekbrains.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.Base2DScreen;
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.Star;
 
 public class MenuScreen extends Base2DScreen {
 
+    private static final int STAR_COUNT = 256;
 
     private Texture bgTexture;
     private Background background;
+
+    private TextureAtlas textureAtlas;
+    private Star[] stars;
 
 
     @Override
@@ -22,27 +28,50 @@ public class MenuScreen extends Base2DScreen {
         super.show();
         bgTexture = new Texture("bg.png");
         background = new Background(new TextureRegion(bgTexture));
+        textureAtlas = new TextureAtlas("menuAtlas.tpack");
+        stars =new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(textureAtlas);
+        }
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        update(delta);
+        draw();
+
+    }
+
+    public void update(float delta) {
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].update(delta);
+        }
+    }
+
+    public void draw() {
         Gdx.gl.glClearColor(0.128f, 0.53f, 0.9f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         background.draw(batch);
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].draw(batch);
+        }
         batch.end();
-
     }
 
     @Override
     public void resize(Rect worldBounds) {
         background.resize(worldBounds);
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].resize(worldBounds);
+        }
     }
 
     @Override
     public void dispose() {
         bgTexture.dispose();
+        textureAtlas.dispose();
         super.dispose();
     }
 
